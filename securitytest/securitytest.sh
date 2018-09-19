ec55d9e34b29bf598f5a3cea2d9446921fbe28b9
Donat Hauser <donat@hauser.world>
2018-09-19 11:13:47 +0200

    Remove previous report

--- securitytest/securitytest.sh
+++ securitytest/securitytest.sh
@@ -4,13 +4,11 @@ SCRIPTDIR=$(dirname $0)
 
 cd $SCRIPTDIR
 
-echo Installing NPM in $SCRIPTDIR
+rm report.html
 
 echo Running Scan
 
 burpctl start
-
-echo curl -k -m 180 https://crimdrac-jhipster-5-demo.herokuapp.com
 
 ./crawl.js
 
